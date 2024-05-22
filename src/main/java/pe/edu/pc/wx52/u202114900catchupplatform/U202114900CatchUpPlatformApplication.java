3fdbcb82e3481ff46ca57a08152dc6c616c7c41c
Sharon Antuanet Ivet Barrial Marin <u202114900@upc.edu.pe>
2024-05-22 13:53:45 -0500

    chore: initial committ

---
+++ src/main/java/pe/edu/pc/wx52/u202114900catchupplatform/U202114900CatchUpPlatformApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.pc.wx52.u202114900catchupplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U202114900CatchUpPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(U202114900CatchUpPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/pc/wx52/u202114900catchupplatform/U202114900CatchUpPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.pc.wx52.u202114900catchupplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U202114900CatchUpPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
